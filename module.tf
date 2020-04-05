c1a28fa1b84ea8635bb9676cdb92d55af7fbfdd8
skalapa <alaparthyshyam@yahoo.com>
2020-04-05 13:04:04 -0500

    initial commit

---
+++ module.tf
@@ -0,0 +1,19 @@
+//--------------------------------------------------------------------
+// Variables
+variable "search_service_hosting_mode" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "search_service" {
+  source  = "app.terraform.io/skalapa-azure/search-service/azurerm"
+  version = "0.0.2"
+
+  client_secret = "${var.client_secret}"
+  hosting_mode = "${var.search_service_hosting_mode}"
+  location = "southcentralus"
+  partition_count = 1
+  replica_count = 1
+  resource_group = "tf-rg-2"
+  search_name = "tfsearch"
+  sku = "free"
+}
